4afa2f918950adbe110ff8dd797827cebfb016c3
Tomas Radvansky <radvansky.tomas@gmail.com>
2017-06-06 21:15:33 +0200

    Data model, query

---
+++ PlusCo/Messages.swift
@@ -0,0 +1,16 @@
+//
+//  Messages.swift
+//  PlusCo
+//
+//  Created by Tomas Radvansky on 06/06/2017.
+//  Copyright © 2017 Olya Danylova. All rights reserved.
+//
+
+import Foundation
+
+class Messages : NSObject
+{
+    var data: String?
+    var to: String?
+    var from: String?
+}
